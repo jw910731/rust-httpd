bbc23a51fe6413c5cd5701a66f3faa8ef1630556
jw910731 <jw910731@gmail.com>
2024-04-02 01:26:37 +0800

    Add comment for main program

--- src/main.rs
+++ src/main.rs
@@ -16,13 +16,25 @@ async fn main() -> Result<()> {
     }));
 
     while let Ok((mut socket, _)) = listener.accept().await {
+        // For each TCP connection
+
+        // Get a ref of HTTP Context
         let ctx = http_context.clone();
+
+        // Spawn a task dedicated to the connection
         pool.spawn(async move {
+            // Split read and write handle
             let (mut rd, mut wr) = socket.split();
+
+            // Get handler object from rw handle
             let mut handler = ctx.get(&mut rd, &mut wr);
             use http::HttpHandleStatus::*;
+
+            // For each http request
             loop {
+                // Deal with the request
                 match handler.handle().await {
+                    // Error occurred
                     Err(e) => {
                         let _ = tokio::io::stderr()
                             .write(
@@ -34,9 +46,11 @@ async fn main() -> Result<()> {
                                 .as_bytes(),
                             )
                             .await
+                            // If async write to stderr fails, fallback to synchronous write
                             .map_err(|e2| eprintln!("Log error failed {}", e2));
                     }
                     Ok(status) => {
+                        // Disconnect if needed
                         if status == EOF {
                             break;
                         }
@@ -44,6 +58,7 @@ async fn main() -> Result<()> {
                 }
             }
         })
+        // Await until task spawned
         .await?;
     }
     Ok(())
